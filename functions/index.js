e058719399155be674c2aed832719f81ae19eaa5
Tanxy Rogue <tanxyrogue@gmail.com>
2020-08-22 10:37:46 +0200

    added sendMail cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +8,44 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.sendUserMessage = functions.firestore
+    .document('emails/{doc_id}')
+    .onCreate((snap, context) => {
+        let name = snap.data().name;
+        let email = snap.data().email;
+        let number = snap.data().number;
+        let message = snap.data().message;
+
+        /* ... */
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'verynicesalonbooker@gmail.com',
+                pass: '1sdfgtre'
+            }
+        });
+
+        let mailOptions = {
+            from: 'verynicesalonbooker@gmail.com',
+            to: 'tanxyrogue@gmail.com',
+            subject: `${name} has left you a message on craig-tanaka.webb.app`,
+            html:   `<h3>The Message</h3>
+                    ${message}<hr><br>
+                    
+                    <h3>User's Details</h3>
+                    <b>Name :</b> ${name}<br>
+                    <b>Email :</b> ${email}<br>
+                    <b>Phone :</b> ${number}`
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+
+        return "Ok"
+});
